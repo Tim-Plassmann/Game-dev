70c3fa6f98fa6467f0e3ebfa81cd143b06a6e46c
tim plassmann <tim.plassmann@datanetservice.dk>
2023-11-21 13:09:27 +0100

    flyve fart

--- Assets/Spaceship/Scenes/executioner.gd
+++ Assets/Spaceship/Scenes/executioner.gd
@@ -1,8 +1,12 @@
 extends Node3D
 
-# Declare variables for movement speed and rotation speed
-var speed: float = 100.0
+#* Declare variables for movement speed and rotation speed
+var speed: float = 0.0
 var rotation_speed: float = 2.0
+var acceleration: float = 5.0
+var max_speed: float = 100.0
+var damping: float = 0.9
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -10,10 +14,17 @@ func _process(delta: float) -> void:
 	var direction = Vector3()  # Initialize a vector to store movement direction
 
 	if Input.is_action_pressed("throttle_up"):
-		direction.z -= 1  # Move forward
+		speed += acceleration * delta
+		speed = min(speed, max_speed)
+		direction.z -= speed # Move forward
+	else:
+		# Apply damping to gradually slow down when no input is detected
+		speed *= pow(damping, delta)
 
 	if Input.is_action_pressed("throttle_down"):
-		direction.z += 1  # Move backward
+		speed -= acceleration * delta
+		speed = max(speed, 0.0)
+		direction.z += speed  # Move backward
 
 	#if Input.is_action_pressed("Roll_left"):
 		#direction.x -= 1  # Strafe left
@@ -36,3 +47,4 @@ func _process(delta: float) -> void:
 	
 	rotate_x(-Input.get_action_strength("Pitch_down") * rotation_speed * delta)
 	rotate_x(Input.get_action_strength("Pitch_up") * rotation_speed * delta)
+	
